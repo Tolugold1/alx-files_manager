606a8d112a866c11a93613d1e7e65d01ea62d032
Tolugold <toluadelekesteve@gmail.com>
2023-03-03 07:56:43 +0100

    update

--- utils/db.js
+++ utils/db.js
@@ -1,38 +1,31 @@
 const { MongoClient } = require('mongodb')
 
-const DB_HOST = process.env.DB_HOST || localhost;
+const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
-const DB_DATABASE = process.env.DB_DATABASE || files_manager;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`
 
 class DBClient {
   constructor() {
-    const url = `mongodb://${DB_HOST}:${DB_PORT}`;
-    const client = new MongoClient(url);
-    client.connect()
-    .then((err, conn) => {
-      if (err) {
-        console.log(err.message);
-        this.db = false;
-        return;
-      }
-
+    this.client = new MongoClient(url);
+    this.client.connect()
+    .then((conn) => {
       this.db = conn.db(DB_DATABASE);
-      this.user = this.db.collection('users');
+      this.users = this.db.collection('users');
       this.files = this.db.collection('files');
+    }, (err) => {
+      console.log(err.message);
+      this.db = false;
+      return;
     })
   };
 
   isAlive() {
-    if (this.db == false) {
-      return false;
-    } else {
-      return true;
-    }
+    return !!this.db;
   };
 
   async nbUsers() {
-    const number_of_users = await this.user.count();
-    return number_of_users;
+    return this.users.countDocuments();
   };
 
   async nbFiles() {
@@ -42,5 +35,5 @@ class DBClient {
 
 };
 
-const dbClient = DBClient();
+const dbClient = new DBClient();
 export default dbClient;
