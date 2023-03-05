d833bf0813bcdf14276735cfab4bbf1ab4b81e96
Tolugold <toluadelekesteve@gmail.com>
2023-03-05 12:13:53 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -7,39 +7,39 @@ import FilesController from "../controllers/FilesController"
 const index = express.Router();
 index.use(bodyParser.json())
 
-index.get('status', (req, res) => {
+index.get('/status', (req, res) => {
   AppController.getStatus(req, res);
 });
 
-index.get('stats', (req, res) => {
+index.get('/stats', (req, res) => {
   AppController.getStats(req, res)
 });
 
-index.get('users' , (req, res) => {
+index.get('/users' , (req, res) => {
   UsersController.postNew(req, res);
 });
 
-index.get('connect', (req, res) => {
+index.get('/connect', (req, res) => {
   AuthController.getConnect(req, res);
 });
 
-index.get('disconnect', (req, res) => {
+index.get('/disconnect', (req, res) => {
   AuthController.getDisconnect(req, res);
 });
 
-index.get('users/me', (req, res) => {
+index.get('/users/me', (req, res) => {
   UsersController.getMe(req, res);
 });
 
-index.post('files', (req, res) => {
+index.post('/files', (req, res) => {
   FilesController.postUpload(req, res);
 });
 
-index.get('files/:id', (req, res) => {
+index.get('/files/:id', (req, res) => {
   FilesController.getShow(req, res);
 });
 
-index.get('files', (req, res) => {
+index.get('/files', (req, res) => {
   FilesController.getIndex(req, res);
 });
 
