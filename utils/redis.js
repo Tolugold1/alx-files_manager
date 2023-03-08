bfd171c0a761b00750511e6cd5cb31d095e75e90
Tolugold <toluadelekesteve@gmail.com>
2023-03-08 15:39:08 +0100

    update

--- utils/redis.js
+++ utils/redis.js
@@ -1,59 +1,60 @@
-import {createClient} from 'redis';
-import { promisify } from 'util';
+import { createClient } from "redis";
+import {promisify} from 'util';
 
 class RedisClient {
-  constructor() {
-    this.r = createClient();
-    this.getClient = promisify(this.r.get).bind(this.r);
-    this.on('error', (err) => {
+
+  constructor () {
+    this.client = createClient();
+    this.getClient = promisify(this.client.get).bind(this.client);
+    this.client.on('error', (err) => {
       console.log(err);
-    });
-  };
+    })
+  }
 
   connect() {
-    return new Promise((res, rej) => {
-      this.r.once('ready', () => {
-        res();
+    return new Promise((resolve, reject) => {
+      this.client.once('ready', () => {
+        resolve();
       });
-  
-      this.r.once('error', (err) => {
-        rej(err);
+
+      this.client.once('error', (error) => {
+        reject(error);
       });
     });
-  };
+  }
 
-  isAlive = () => {
-    if (this.r.connected == true) {
+  isAlive () {
+    if(this.client.connected == true) {
       return true;
     }
     try {
       this.connect();
       return true;
-    } catch (err) {
-      console.log(`Redis client not connected to the server: ${err}`)
+    } catch (error) {
+      console.error('Error connecting to Redis:', error);
       return false;
     }
-  };
+  }
 
   async get(key) {
     if (this.isAlive() == true) {
-      const value_stored = await this.getClient(key);
-      return value_stored;
+      return this.getClient(key);
     }
-  };
+  }
 
   async set(key, value, duration) {
-    if (this.isAlive() == true) {
-      this.r.setex(key, duration, value);
+    if(this.isAlive() == true) {
+      this.client.setex(key, duration, value);
     }
-  };
+  }
 
   async del(key) {
-    if (this.isAlive() == true) {
-      this.r.del(key);
+    if(this.isAlive() == true) {
+      let res = this.client.del(key);
+      return res;
     }
-  };
-};
+  }
+}
 
 const redisClient = new RedisClient();
 
