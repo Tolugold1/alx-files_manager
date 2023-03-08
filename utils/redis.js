90a53b07b998120d4e8a8efa7832af63f5052b9a
Tolulope Adeleke <toluadelekesteve@gmail.com>
2023-03-08 06:10:16 -0800

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,52 +1,65 @@
-import {createClient} from 'redis';
-import { promisify } from 'util';
+import { createClient } from "redis";
+import {promisify} from 'util';
 
 class RedisClient {
-  constructor() {
-    this.r = createClient();
-    this.getClient = promisify(this.r.get).bind(this.r);
-  };
+
+  constructor () {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.log(err);
+    })
+  }
 
   connect() {
-    return new Promise((res, rej) => {
-      this.r.on('ready', () => {
-        res();
+    return new Promise((resolve, reject) => {
+      this.client.once('ready', () => {
+        resolve();
       });
-  
-      this.r.on('error', (err) => {
-        rej(Error(`Redis client not connected to the server: ${err}`));
+
+      this.client.once('error', (error) => {
+        reject(error);
       });
     });
-  };
+  }
 
-  isAlive = () => {
+  isAlive () {
+    if(this.client.connected == true) {
+        return true;
+    }
     try {
       this.connect();
       return true;
-    } catch {
+    } catch (error) {
+      console.error('Error connecting to Redis:', error);
       return false;
     }
-  };
+  }
+
 
   async get(key) {
-    if (this.isAlive() == true) {
-      const value_stored = await this.getClient(key);
-      return value_stored;
+    if(this.isAlive() == true) {
+      const getProm = promisify(this.client.get).bind(this.client);
+      let res = await getProm(key);
+      return res;
     }
-  };
+  }
 
   async set(key, value, duration) {
-    if (this.isAlive() == true) {
-      this.r.setex(key, duration, value);
+    if(this.isAlive() == true) {
+      const setProm = promisify(this.client.setex).bind(this.client);
+      await setProm(key, duration, value);
     }
-  };
+  }
 
   async del(key) {
-    if (this.isAlive() == true) {
-      this.r.del(key);
+    if(this.isAlive() == true) {
+      const delProm = promisify(this.client.del).bind(this.client);
+      let res = await(delProm(key));
+      return res;
     }
-  };
-};
+  }
+}
 
 const redisClient = new RedisClient();
+
 export default redisClient;
